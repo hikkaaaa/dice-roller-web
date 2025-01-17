e02c1b4de4bda07a045445c2aeca535be4f1fab9
hikkaaaa <123duuuuum@gmail.com>
2025-01-17 14:37:11 +0500

    Add files via upload

---
+++ dice_roller.js
@@ -0,0 +1,16 @@
+function rollDice(){
+    const userInput = document.getElementById("myInput").value;
+    const diceResult = document.getElementById("dice-array-answer");
+    const diceImages = document.getElementById("dice-image");
+
+    const result = [];
+    const images = [];
+
+    for(let i = 1; i <= userInput; i++){
+        const randomNumber = Math.floor(Math.random() * 6) + 1; 
+        result.push(randomNumber);
+        images.push(`<img src = "./dice-images/side${randomNumber}.jpg" alt = "Dice ${randomNumber}">`);
+    }
+    diceResult.textContent = "dice: " + [...result]; 
+    diceImages.innerHTML = images.join('');
+}
